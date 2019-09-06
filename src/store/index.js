17b775b987fd5404740e1ff817d8278375d99055
mshayanabbasi <mshayanabbasi>
2019-09-06 15:41:22 +0500

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
 import rootReducers from "../reducers/index";
+import middleware from '../middleware'
 
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, middleware)
 export default store
 
 
