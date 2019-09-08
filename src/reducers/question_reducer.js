8d78ca5a965ef1132b420def3607abcdc18274af
mshayanabbasi <mshayanabbasi>
2019-09-08 14:18:11 +0500

    question reducer file

---
+++ src/reducers/question_reducer.js
@@ -0,0 +1,26 @@
+import { INITIAL_DATA, SAVE_QUESTION, SAVE_QUESTION_ANSWER, GET_QUESTIONS } from '../actions/actionCreators'
+
+
+const question_reducer = ( state = {}, action ) => {
+    switch (action.type) {
+        case GET_QUESTIONS:
+            return action.questions
+        case SAVE_QUESTION:
+            return {
+                ...state,
+                [action.qid]: {
+                    ...state[action.qid],
+                    [action.answer]: {
+                        ...state[action.qid][action.answer],
+                        votes: [...state[action.qid][action.answer].votes, action.authedUser]
+                    }
+                }
+            }
+        case INITIAL_DATA:
+            return action.questions
+        default:
+            return state
+    }
+}
+
+export default question_reducer
