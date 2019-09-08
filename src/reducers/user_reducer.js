eb13f8ea3b94f8a4697d3898b50e68f78eb088db
mshayanabbasi <mshayanabbasi>
2019-09-08 14:25:46 +0500

    users reducer file

---
+++ src/reducers/user_reducer.js
@@ -0,0 +1,33 @@
+import { INITIAL_DATA, GET_USERS, SAVE_QUESTION, SAVE_QUESTION_ANSWER } from '../actions/actionCreators'
+
+const user_reducer = ( state = [], action ) => {
+    switch (action.type) {
+        case GET_USERS:
+            return action.users
+        case INITIAL_DATA:
+            return action.users
+        case SAVE_QUESTION_ANSWER:
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    answers: {
+                        ...state[action.authedUser]['answers'],
+                        [action.qid]: action.answer
+                    }
+                }
+            }
+        case SAVE_QUESTION:
+            return {
+                ...state,
+                [action.question.author]: {
+                    ...state[action.question.author],
+                    questions: [...state[action.question.author].questions, action.question.id]
+                }
+            }
+        default:
+            return state
+    }
+}
+
+export default user_reducer
