51f4735e23ab713889587e726adcb7bb910ca0e1
mshayanabbasi <mshayanabbasi>
2019-09-08 14:11:23 +0500

    login reducer

---
+++ src/reducers/login_reducer.js
@@ -0,0 +1,17 @@
+import { LOGIN_USER, LOGOUT_USER, INITIAL_DATA } from '../actions/actionCreators'
+
+
+const login_reducer = ( state = [], action ) => {
+    switch (action.type) {
+        case LOGIN_USER:
+            return action.authedUser
+        case LOGOUT_USER:
+            return false
+        case INITIAL_DATA:
+            return false
+        default:
+            return state
+    }
+}
+
+export default login_reducer
