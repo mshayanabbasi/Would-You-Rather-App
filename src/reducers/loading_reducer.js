9732d176b9fe29f43c770d9f2689b1589021596d
mshayanabbasi <mshayanabbasi>
2019-09-08 13:47:13 +0500

    loading reducer file

---
+++ src/reducers/loading_reducer.js
@@ -0,0 +1,12 @@
+import { INITIAL_DATA } from '../actions/actionCreators'
+
+const loading_reducer = ( state = true, action ) => {
+    switch (action.type) {
+        case INITIAL_DATA:
+            return false
+        default:
+            return state
+    }
+}
+
+export default loading_reducer
