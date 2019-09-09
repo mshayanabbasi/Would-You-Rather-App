990e0d6cad5d4735b0fd49da2c3b47bc3b31b843
mshayanabbasi <mshayanabbasi>
2019-09-09 11:29:30 +0500

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux'
 import rootReducer from '../reducers'
-
-const store = createStore(rootReducer)
+import middleware from '../middleware'
+const store = createStore(rootReducer, middleware)
 
 export default store
 
