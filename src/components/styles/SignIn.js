575f17d686285d7e314ca10431156cb493fbbd4a
mshayanabbasi <mshayanabbasi>
2019-09-09 11:28:52 +0500

    styles

---
+++ src/components/styles/SignIn.js
@@ -0,0 +1,12 @@
+const styles = theme => ({
+    centerScreen: {
+        display: 'flex',
+        flexDirection: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        minHeight: '100vh',
+        backgroundColor: '#008080'
+    }
+})
+
+export default styles
