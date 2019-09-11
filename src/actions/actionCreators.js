b8eb926a8b705c14d79be9fa910c59bca89d0f2d
mshayanabbasi <mshayanabbasi>
2019-09-11 13:32:22 +0500

    done!

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,60 +1,45 @@
-const SAVE_QUESTION = 'SAVE_QUESTION'
-const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER'
-const GET_USERS = 'GET_USERS'
-const INITIAL_DATA = 'INITIAL_DATA'
-const GET_QUESTIONS = 'GET_QUESTIONS'
-const LOGIN_USER = 'LOGIN_USER'
-const LOGOUT_USER = 'LOGOUT_USER'
+export const SAVE_QUESTION = 'SAVE_QUESTION'
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER'
+export const GET_USERS = 'GET_USERS'
+export const INITIAL_DATA = 'INITIAL_DATA'
+export const GET_QUESTIONS = 'GET_QUESTIONS'
+export const LOGIN_USER = 'LOGIN_USER'
+export const LOGOUT_USER = 'LOGOUT_USER'
 
 
-const initialDataActionCreator = (users, questions) => ({
+export const initialDataActionCreator = (users, questions) => ({
     type: INITIAL_DATA,
     users,
     questions
 })
 
-const authedUserActionCreator = (authedUser) => ({
+export const authedUserActionCreator = (authedUser) => ({
     type: LOGIN_USER,
     authedUser
 })
 
-const userLogoutActionCreator = () => ({
+export const userLogoutActionCreator = () => ({
     type: LOGOUT_USER
 })
 
-const saveQuestionActionCreator = (question) => ({
-    type: SAVE_QUESTION,
-    question
-})
-
-const getUserActionCreator = (users) => ({
+export const getUserActionCreator = (users) => ({
     type: GET_USERS,
     users
 })
 
-const getQuestionActionCreator = (questions) => ({
+export const getQuestionActionCreator = (questions) => ({
     type: GET_QUESTIONS,
     questions
 })
-const saveQuestionAnswerActionCreator = (authedUser, qid, answer) => ({
+
+export const saveQuestionActionCreator = (question) => ({
+    type: SAVE_QUESTION,
+    question
+})
+
+export const saveQuestionAnswerActionCreator = (authedUser, qid, answer) => ({
     type: SAVE_QUESTION_ANSWER,
     authedUser,
     qid,
     answer
 })
-export {
-    SAVE_QUESTION,
-    SAVE_QUESTION_ANSWER,
-    INITIAL_DATA,
-    GET_QUESTIONS,
-    GET_USERS,
-    LOGIN_USER,
-    LOGOUT_USER,
-    saveQuestionActionCreator,
-    saveQuestionAnswerActionCreator,
-    getQuestionActionCreator,
-    getUserActionCreator,
-    userLogoutActionCreator,
-    initialDataActionCreator,
-    authedUserActionCreator
-}
