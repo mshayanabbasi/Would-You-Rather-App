3b7051691796cabc42ac0546f8bf7733de6967dd
mshayanabbasi <mshayanabbasi>
2019-09-11 13:34:26 +0500

    done!

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,8 +1,11 @@
 const logger = (store) => (next) => (action) => {
     console.group(action.type)
-    console.log('The action ', action)
+    console.log('Time to complete transaction')
+    console.log('App current state: ', store.getState())
+    console.log('Action dispatched: ', action)
     const response = next(action)
-    console.log('New State: ', store.getState())
+    console.log('App updated state: ', store.getState())
+    console.log('Time to complete transaction')
     console.groupEnd()
     return response
 }
